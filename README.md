52f51394e2a9bb991e8d11823395fa4c4214c764
SamiraMHosseini <samiramhosseini19@gmail.com>
2023-08-20 11:24:37 -0500

    Add project files.

---
+++ README.md
@@ -0,0 +1,24 @@
+# Thread-Safe File Logging in C++: Best Practices and Common Pitfalls
+This repository demonstrates how to perform file logging in a multithreaded C++ application, focusing on best practices for thread-safe operations and common pitfalls that can lead to race conditions and data corruption.
+
+# Contents
+Race Conditions: Examples that highlight where race conditions can occur in multithreaded file logging.
+
+Using Mutexes: Demonstrates how to use mutexes to synchronize file access across different threads.
+
+Anti-Patterns: Illustrates dangerous practices that should be avoided to prevent data races.
+
+# Race Conditions
+Race conditions occur when multiple threads access shared resources simultaneously, leading to unpredictable and undesirable behavior. In the context of file logging, this can result in interleaved or corrupted file content.
+
+# Using Mutexes
+Mutexes are used to ensure that only one thread at a time can write to a file, preventing the aforementioned race conditions. This section demonstrates how to use std::lock_guard with std::mutex to manage access to shared resources.
+# Anti-Patterns
+Certain practices, if not handled correctly, can lead to data races and other synchronization issues. This section focuses on what not to do when dealing with multithreaded file logging.
+1. # Exposing Internal Resources
+   In the code, the method get_ofstream returns a reference to the internal std::ofstream object. This exposes the internal resource to the outside world, allowing unauthorized access to it.
+   This is an anti-pattern because it bypasses the mutex that is supposed to control access to the ofstream. Once an external user gets a reference to the ofstream,
+   they can write to it directly without going through the lock, leading to potential race conditions.
+2. # Bypassing Mutexes
+   The process_ofstream method takes a function pointer that accepts an ofstream reference, and it directly passes the internal ofstream to that function without going through the mutex.
+   This is an anti-pattern because it completely bypasses the lock mechanism and allows unauthorized manipulation of the ofstream object, as demonstrated in the malicious function. This approach can lead to data corruption     and race conditions.
---
+++ SandBox/main.cpp
@@ -0,0 +1,126 @@
+#include <filesystem>
+#include <fstream>
+#include <thread>
+#include <mutex>
+
+#if 1
+
+
+#include <fstream>
+
+// Malicious function that can print to ofs without going through the lock
+void malicious(std::ofstream& ofs)
+{
+	std::string filename = "F:\\info.txt";
+	ofs = std::ofstream(filename, std::ofstream::out);
+	if (!ofs.is_open())
+	{
+		ofs.open(filename);
+	}
+	for (int i = 0; i <= 100; ++i)
+	{
+		ofs << "This is a malicious act " << i << '\n'; // Writing directly to the file
+		std::string msg = "From t1: " + std::to_string(i) + '\n';
+		ofs.write(msg.data(), msg.length()); // Writing directly to the file
+	}
+}
+
+struct LogFile
+{
+	LogFile(std::string_view filename_) :
+		mtx(),
+		ofs(),
+		filename(filename_)
+	{
+		// Constructor initializes members but does not open the file
+	}
+
+	void write_into_file_V2(std::string_view str)
+	{
+
+		std::call_once(this->openfile_flag, [&]() {
+
+			// Opening the file if not already open
+			this->ofs = std::ofstream(this->filename, std::ofstream::out);
+
+			});
+
+		std::lock_guard<std::mutex> lock(this->mtx); // Locking to ensure thread safety
+		this->ofs << str << '\n'; // Writing the string to the file
+	}
+
+	void write_into_file(const std::string& str)
+	{
+		// This will be called only once and only by one thread as we only need to open the file once
+		std::call_once(this->openfile_flag, [&]() {
+
+			// Opening the file if not already open
+			this->ofs = std::ofstream(this->filename, std::ofstream::out);
+			});
+
+		std::lock_guard<std::mutex> lock(this->mtx); // Locking to ensure thread safety
+		this->ofs.write(str.data(), str.length()); // Writing the string to the file
+	}
+
+	// WARNING: Returning a reference to ofs allows direct access to the stream
+	// without going through the lock, potentially leading to race conditions
+	// Exposing Internal Resources
+	std::ofstream& get_ofstream()
+	{
+		return this->ofs;
+	}
+
+	// This function takes a user-provided function and applies it to the ofstream
+	// This is risky as it allows the user to bypass the mutex and directly manipulate the file
+	// Bypassing Mutexes
+	void process_ofstream(void (*func_ptr)(std::ofstream& ofs))
+	{
+		func_ptr(this->ofs); // Calling the user-provided function with the ofstream
+	}
+
+	~LogFile()
+	{
+		this->ofs.close(); // Closing the file when the LogFile object is destroyed
+	}
+
+private:
+	std::mutex mtx;					// Mutex for synchronizing access to the file
+	std::once_flag openfile_flag;   // We only need to open a file once during execution of a program
+	std::ofstream ofs;				// The output file stream (the resource)
+	std::string filename;			// The name of the file
+};
+
+void function_1(LogFile& logFile)
+{
+	for (int i = 0; i > -100; --i)
+	{
+		std::string msg = "From t1: " + std::to_string(i) + '\n'; // Message to be written
+		logFile.write_into_file(msg);    // Writing using method 1
+		logFile.write_into_file_V2(msg); // Writing using method 2
+	}
+}
+
+void function_2()
+{
+	for (int i = 400; i <= 500; i += 1)
+	{
+		std::cout << "From t2: " << i << '\n'; // Printing to standard output
+	}
+}
+
+int main()
+{
+	LogFile logFile("C:\\Interview Summer 2023\\samira.txt");
+	std::jthread t1(function_1, std::ref(logFile));
+	std::jthread t2(function_2);
+	logFile.process_ofstream(malicious); // Invoking the malicious function
+	for (int i = 0; i < 100; ++i)
+	{
+		std::string msg = "From main: " + std::to_string(i) + '\n'; // Message to be written
+		logFile.write_into_file(msg);    // Writing using method 1
+		logFile.write_into_file_V2(msg); // Writing using method 2
+	}
+}
+
+
+#endif
